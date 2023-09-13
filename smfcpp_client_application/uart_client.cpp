f257196fe71a7529917327a93481c15f53432474
littsk <1214689160@qq.com>
2023-09-12 22:31:30 -0700

    Update uart_client.cpp

--- smfcpp_client_application/uart_client.cpp
+++ smfcpp_client_application/uart_client.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char * argv[])
 {
     auto uart_client = smfcpp::UartClient::make_shared(
-        "uart_client", "119.29.251.32", 9000, "/dev/ttyUSB0", 1);
+        "uart_client", "119.29.251.32", 9000, "/dev/ttyUSB0", 30);
     uart_client->connect_to_server();
     smfcpp::SingleThreadedExecutor exec;
     exec.add_node(uart_client);
